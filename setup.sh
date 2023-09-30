c91dd5b9e34d99d83726c43afd68b50336c3c841
Bhanu Pratap Singh Rathore <bhanur05@gmail.com>
2023-09-30 21:56:18 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+wget https://repo.anaconda.com/archive/Anaconda3-2023.07-2-Linux-x86_64.sh
+chmod +x Anaconda3-2023.07-2-Linux-x86_64.sh
+./Anaconda3-2023.07-2-Linux-x86_64.sh
+eval "$(/root/anaconda3/bin/conda shell.bash hook)"
